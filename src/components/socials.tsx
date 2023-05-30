c5b5714419a749c1d7f14aab63be4a31b7385265
Andrew LC <smartandrew2003@gmail.com>
2023-05-31 00:17:17 +0530

    Icons fix

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -6,14 +6,14 @@ const links = {
 
 export default function Socials() {
     return (
-        <div className="flex gap-3 mt-4 cursor-pointer text-2xl">
-            <a href={links.twitter}>
+        <div className="flex gap-3 mt-4 text-2xl">
+            <a href={links.twitter} className="cursor-pointer">
                 <i className='bx bxl-twitter'></i>
             </a>
-            <a href={links.linkedin}>
+            <a href={links.linkedin} className="cursor-pointer">
                 <i className='bx bxl-linkedin-square' ></i>
             </a>
-            <a href={links.github}>
+            <a href={links.github} className="cursor-pointer">
                 <i className='bx bxl-github' ></i>
             </a>
         </div>
