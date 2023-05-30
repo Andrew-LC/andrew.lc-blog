5f18c9367505426b3511b02270856b7feb840ffe
Andrew LC <smartandrew2003@gmail.com>
2023-05-31 00:13:45 +0530

    Icons fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Socials from "./socials";
 
 export default function Footer() {
     return (
-        <header className="w-full flex flex-col lg:flex-row lg:justify-center items-center justify-evenly bg-[#f5f5f4] p-4 text-[20px] text-regular">
+        <header className="w-full flex flex-col lg:flex-row lg:justify-center items-center justify-evenly bg-[#f5f5f4] p-4 lg:text-[20px] text-regular">
             <span>Copyright &#169; 2023 | All rights reserved.</span>
             <Socials />
         </header>
