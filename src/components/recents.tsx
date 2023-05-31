6f19540f5229841f4e212f8e9ea6dacd99514bf0
Andrew LC <smartandrew2003@gmail.com>
2023-05-31 15:15:18 +0530

    api added and post folder

--- src/components/recents.tsx
+++ src/components/recents.tsx
@@ -1,37 +1,30 @@
-"use client"
-import { useEffect, useState } from "react";
 import { getAllPosts } from "../lib/api";
 import Link from "next/link";
 
-export default function Recents() {
-    const [articles, setArticles] = useState([]);
-
-    useEffect(() => {
-        async function fetchArticles() {
-            const res = getAllPosts(["title", "date", "slug"]);
-            setArticles(res);
-        }
-
-        fetchArticles();
-    }, []);
-
+export default async function Recents() {
+    const res = await getAllPosts([
+        'title',
+        'date',
+        'slug'
+    ])
     return (
         <div className="flex flex-col gap-2 mt-6">
             <h2 className="font-bold text-2xl mb-2"># Recents</h2>
             <div>
-                {articles.map((article, index) => (
-                    <div key={index} className="text-lg">
-                        <span className="mr-3 font-bold text-[#625757]">
-                            {article.date}
-                        </span>
-                        <Link href={`/post/${article.slug}`}>
-                            <span className="volkh-font text-black text-[1.2rem] hover:underline cursor-pointer">
-                                {article.title}
-                            </span>
-                        </Link>
-                    </div>
-                ))}
+                {
+                    res.map((article, index) => {
+                        return (
+                            <div key={index} className="text-lg" >
+                                <span className="mr-3 font-bold text-[#625757]">{article.date}</span>
+                                <Link href={`/post/${article.slug}`}>
+                                    <span className="volkh-font text-black text-[1.2rem] hover:underline cursor-pointer">{article.title}</span>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
             </div>
-        </div>
+        </div >
     );
 }
+
