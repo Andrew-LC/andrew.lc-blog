2a8edc204b32313ea28b86b032b64132a25b2557
Andrew LC <smartandrew2003@gmail.com>
2023-05-31 19:56:42 +0530

    post page fix

--- lib/orgToHtml.ts
+++ lib/orgToHtml.ts
@@ -4,11 +4,17 @@ import html from 'rehype-stringify';
 import uniorg from 'uniorg-parse';
 import uniorg2rehype from 'uniorg-rehype';
 import extractKeywords from 'uniorg-extract-keywords';
+import highlight from 'rehype-highlight';
+import katex from 'rehype-katex';
 
 const processor = unified()
   .use(uniorg)
   .use(extractKeywords)
   .use(uniorg2rehype)
+  //@ts-ignore
+  .use(highlight)
+  //@ts-ignore
+  .use(katex)
   //@ts-ignore
   .use(html);
 
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -16,10 +16,10 @@ export default function Post({ params }: { params: { slug: string } }) {
         "content"
     ] as metaData) as Partial<metaData>
     return (
-        <div className="flex-1 pt-24 pb-16 w-full p-4 lg:w-[740px] mx-auto">
-            <h1 className="font-extrabold text-6xl mb-2">{res.title}</h1>
+        <div className="flex-1 pt-24 pb-16 w-full pr-6 pl-6 lg:pr-0 lg:pl-0 lg:w-[740px] mx-auto">
+            <h1 className="font-extrabold tracking-tight text-3xl lg:text-6xl mb-2">{res.title}</h1>
             <span className="text-stone-500 font-semibold">Posted on {res.date}</span>
-            <div className="prose prose-lg prose-p:volkh-font prose-stone max-w-none mt-5" dangerouslySetInnerHTML={{ __html: res.content as TrustedHTML }} />
+            <div className="prose prose-lg prose-p:volkh-font prose-zinc max-w-none mt-5" dangerouslySetInnerHTML={{ __html: res.content as TrustedHTML }} />
         </div>
     );
 }
