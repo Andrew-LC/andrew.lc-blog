e47a7f3b19d512e9a674693bfcd8b7ef3d22064c
Andrew LC <smartandrew2003@gmail.com>
2023-05-31 19:40:59 +0530

    post page fix

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -16,10 +16,10 @@ export default function Post({ params }: { params: { slug: string } }) {
         "content"
     ] as metaData) as Partial<metaData>
     return (
-        <div className="flex-1 pt-24 pb-16 w-[740px] mx-auto">
+        <div className="flex-1 pt-24 pb-16 w-full p-4 lg:w-[740px] mx-auto">
             <h1 className="font-extrabold text-6xl mb-2">{res.title}</h1>
             <span className="text-stone-500 font-semibold">Posted on {res.date}</span>
-            <div className="prose prose-lg prose-p:volkh-font prose-stone w-full max-w-none mt-5" dangerouslySetInnerHTML={{ __html: res.content as TrustedHTML }} />
+            <div className="prose prose-lg prose-p:volkh-font prose-stone max-w-none mt-5" dangerouslySetInnerHTML={{ __html: res.content as TrustedHTML }} />
         </div>
     );
 }
