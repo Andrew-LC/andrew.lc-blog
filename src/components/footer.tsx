91f1a8ffc21d6014349f922b5c6008caca0caf63
Andrew LC <smartandrew2003@gmail.com>
2023-06-05 17:36:57 +0530

    simple change

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,9 @@ import Socials from "./socials";
 
 export default function Footer() {
     return (
-        <header className="bg-light-header dark:bg-dark-header text-font-color dark:text-dark-font w-full flex flex-col lg:flex-row justify-center items-center lg:justify-evenly bg-[#f5f5f4] p-4 text-regular">
+        <footer className="bg-light-header dark:bg-dark-header text-font-color dark:text-dark-font w-full flex flex-col lg:flex-row justify-center items-center lg:justify-evenly bg-[#f5f5f4] p-4 text-regular">
             <span>Copyright &#169; 2023 | All rights reserved.</span>
             <Socials />
-        </header>
+        </footer>
     );
 }
